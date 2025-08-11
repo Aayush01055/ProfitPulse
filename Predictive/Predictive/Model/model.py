1ca3e1c738668199f2dacc5ad3a0b4e946343e85
Aayush Shah <82150675+Aayush01055@users.noreply.github.com>
2025-08-11 22:23:35 +0530

    Update model.py

--- Predictive/Predictive/Model/model.py
+++ Predictive/Predictive/Model/model.py
@@ -40,7 +40,7 @@ app.config['MAIL_SERVER'] = "smtp.gmail.com"
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = "aayushshah342@gmail.com"
-app.config['MAIL_PASSWORD'] = "dqpz bbev rmge ptku"
+app.config['MAIL_PASSWORD'] = "APP_PASSWORD"
 mail = Mail(app)
 
 # Firm Prediction Model
